9bad754e355cd80f6b5cb75e99094b4c77db4384
Ibrahim Ahmed <ibrahimahmed3647@gmail.com>
2023-03-03 23:29:08 +0500

    Update Bonus Task.dart

--- Assignments of Dart & Flutter/Week 1 Assignment/Bonus Task.dart
+++ Assignments of Dart & Flutter/Week 1 Assignment/Bonus Task.dart
@@ -1,9 +1,20 @@
 import 'dart:io';
 
-main(){
-  print("Enter a number");
-  dynamic num = int.parse(stdin.readLineSync()!);
-  if (num % 2 != 0 && num ) {
-    
+bool isPrime(N) {
+  for (var i = 2; i <= N / i; ++i) {
+    if (N % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+void main() {
+  print('Enter Any Number');
+  var N = int.parse(stdin.readLineSync()!);
+  if (isPrime(N)) {
+    print('$N is a prime number.');
+  } else {
+    print('$N is not a prime number.');
   }
 }
